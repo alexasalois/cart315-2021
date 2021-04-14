da05143c2d0395773a6df01a0d202365cb46605d
alex salois <43211483+alexasalois@users.noreply.github.com>
2021-04-14 18:43:32 -0400

    FINAL GAME
    adding score / lives

---
+++ ASalois_FINALGAME/Assets/backgroundScroll.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScroll : MonoBehaviour
+{
+
+    public BoxCollider2D collider;
+    public Rigidbody2D rb;
+
+    private float width;
+    private float scrollSpeed = -5f;
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = collider.size.x;
+        collider.enabled = false;
+
+      //  rb.velocity = new Vector2(scrollSpeed, 0);
+
+        ResetObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      rb.velocity = new Vector2(scrollSpeed, 0);
+
+
+        if (transform.position.x < -width)
+        {
+          Vector2 resetPosition = new Vector2(width * 2f,0);
+          transform.position = (Vector2)transform.position + resetPosition;
+          ResetObstacle();
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer > 6f)
+        {
+          scrollSpeed -= 2;
+          timer = 0;
+        }
+
+    }
+
+    void ResetObstacle() {
+        transform.GetChild(0).localPosition = new Vector3(0, Random.Range(-4,4),0);
+    }
+}
---
+++ ASalois_FINALGAME/Assets/playerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float timer;
+    public float score;
+
+    public Rigidbody2D rb;
+    public BoxCollider2D collider;
+
+    public float moveSpeed = 10;
+    public float playerLife = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update function...");
+        float moveDirection = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(0, moveDirection * moveSpeed);
+
+      //  float moveDirectionSide = Input.GetAxisRaw("Horizontal");
+      //  rb.velocity = new Vector3(moveDirectionSide, 0 * moveSpeed);
+
+            timer += Time.deltaTime;
+            if (timer > 1f)
+            {
+              score += 1;
+              timer = 0;
+              Debug.Log(score);
+            }
+          }
+
+      void OnTriggerEnter2D(Collider2D other)
+      {
+          Debug.Log("this is the collision trigger");
+          if(other.gameObject.CompareTag("Obstacle"))
+          {
+            playerLife -= 1;
+            Debug.Log(playerLife);
+          }
+
+          if(playerLife == 0)
+          {
+            Destroy(gameObject);
+          }
+        }
+      }
